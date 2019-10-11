dbd3334055dea5a7baaaaf1b9d31bfd61c6033d5
Panela Leung <leungpanela@gmail.com>
2019-10-11 03:37:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,10 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
-  
+
+  kickstarter.each do |stuff|
+    binding.pry
+  end
 end
 
 # create_project_hash
